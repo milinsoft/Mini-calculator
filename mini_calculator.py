7bf76fd7bbcbaa29b1ba87a1fcc8e73f0a7cea06
parseltongist <alex.milinkevich@gmail.com>
2021-07-02 13:44:49 +0200

    stage 3/4

--- mini_calculator.py
+++ mini_calculator.py
@@ -1,3 +1,4 @@
+# write your code here
 import random
 
 
@@ -21,16 +22,34 @@ def math_task_generator():
     return str(arg1) + " " + operator + " " + str(arg2)
 
 
-# assigning the result of math_task_generator() to a variable to make sure we compare the same expression, not a new one
-random_expression = math_task_generator()
-# printing the randomly generated expression so user can see it
-print(random_expression)
+def user_input_check():
+    # this function taking user input and if it's not valid (not int) prints an error message and ask to try again
+    # returns user_answer if it's ok
+    try:
+        user_answer = int(input())
+    except:
+        print("Incorrect format.")
+        return user_input_check()
+    else:
+        return user_answer
 
-# program reads user's answer
-user_answer = int(input())
 
-# comparing user's answer with the result of computer's calculation and print "Right!" or "Wrong" respectively
-if user_answer == calculation(random_expression):
-    print("Right!")
-else:
-    print("Wrong!")
+
+mark = 5
+
+# a user will be given 5 tasks to solve, that's why the loop is created.
+for _ in range(5):
+    # assigning the result of math_task_generator() to a variable to make sure we compare the same expression, not a new one
+    random_expression = math_task_generator()
+    # printing the randomly generated expression so user can see it
+    print(random_expression)
+    # program reads user's answer invoking the user_input_check() function.
+    # function gives a possibility to rerun only user input, not the whole loop
+    answer = user_input_check()
+    # comparing user's answer with the result of computer's calculation and print "Right!" or "Wrong" respectively
+    if answer == calculation(random_expression):
+        print("Right!")
+    else:
+        print("Wrong!")
+        mark -= 1
+print(f"Your mark is {mark}/5.")
