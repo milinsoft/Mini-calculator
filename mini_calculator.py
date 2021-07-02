9a84c47a6f19bdd0b3b0e0c6065cfdda47242b92
parseltongist <alex.milinkevich@gmail.com>
2021-07-02 13:20:05 +0200

    stage 2/4

--- mini_calculator.py
+++ mini_calculator.py
@@ -1,15 +1,36 @@
+import random
+
+
 def calculation(expr):
-    arg1, op, arg2 = expr.split()
+    arg1, operator, arg2 = expr.split()
     arg1, arg2 = int(arg1), int(arg2)
-    if op == "+":
+    if operator == "+":
         return arg1 + arg2
-    elif op == "-":
+    elif operator == "-":
         return arg1 - arg2
-    elif op == "*":
+    elif operator == "*":
         return arg1 * arg2
     else:
         return "Error. Division should be skipped in this stage. Division operand found"
 
-expression = input()
 
-print(calculation(expression))
+def math_task_generator():
+    arg1 = random.randint(2, 9)
+    operator = random.choice("+-*")
+    arg2 = random.randint(2, 9)
+    return str(arg1) + " " + operator + " " + str(arg2)
+
+
+# assigning the result of math_task_generator() to a variable to make sure we compare the same expression, not a new one
+random_expression = math_task_generator()
+# printing the randomly generated expression so user can see it
+print(random_expression)
+
+# program reads user's answer
+user_answer = int(input())
+
+# comparing user's answer with the result of computer's calculation and print "Right!" or "Wrong" respectively
+if user_answer == calculation(random_expression):
+    print("Right!")
+else:
+    print("Wrong!")
